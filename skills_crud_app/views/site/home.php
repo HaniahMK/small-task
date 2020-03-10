09c565d8017a34638b79a404393786c530bfce5e
HaniahMK <almalki.haniah@gmail.com>
2020-03-10 11:04:39 +0200

    App: Add home page

---
+++ skills_crud_app/views/site/home.php
@@ -0,0 +1,24 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Small Task Application';
+?>
+<div class="site-home">
+
+    <div >
+
+
+        <p class="lead">Skills CRUD Application</p>
+
+        <p><a class="btn btn-lg btn-success" href="/skill">View Skills</a></p>
+
+        <p><a class="btn btn-lg btn-success" href="/description">View Description</a></p>
+    </div>
+
+    <div class="body-content">
+
+
+
+    </div>
+</div>
